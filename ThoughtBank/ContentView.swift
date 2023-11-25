7df53ce355188cf83e6d93cf7a5b51b9c2a1649b
Kranti <darkphoton20@gmail.com>
2023-11-26 02:03:25 +0800

    Initial Commit

---
+++ ThoughtBank/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ThoughtBank
+//
+//  Created by Kranti on 2023/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ThoughtBank/ThoughtBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThoughtBankApp.swift
+//  ThoughtBank
+//
+//  Created by Kranti on 2023/11/26.
+//
+
+import SwiftUI
+
+@main
+struct ThoughtBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
